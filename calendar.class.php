0f2c5df83d0a20cf0ad778dbc42b04bb8acbf257
Greg Nicholas <greg.nicholas@gmail.com>
2011-09-26 18:58:57 -0400

    Working version of calendar with nav links and AJAX

--- calendar.class.php
+++ calendar.class.php
@@ -1,118 +1,144 @@
 <?php
 
-// TODO: extract GET args
-
 require_once('etc/config.php');
 
-$input = date('Y-m-d');
-
 class TwoWeekCalendar {
-	public function __construct($focus_date_string) {
-		$this->focus_date = new DateTime($focus_date_string);
+	public function __construct($anchor_date_string) {
+		$this->anchor_date = new DateTime($anchor_date_string);
 
-		$dt_iter = clone $this->focus_date;
+		$dt_iter = clone $this->anchor_date;
 		$day_interval = new DateInterval('P1D');
 
 		// determine the beginning and end of the two week interval
 		while($dt_iter->format('N')!='7') {
 			$dt_iter->sub($day_interval);
 		}
-		$start_date = clone $dt_iter;
+		$this->start_date = clone $dt_iter;
 
 		$cal_end_str = $dt_iter->add(new DateInterval('P14D'))->format('Y-m-d');
 
-		$events = $this->findAllEvents($start_date->format('Y-m-d'),$cal_end_str);
+		$events = $this->findAllEvents($this->start_date->format('Y-m-d'),$cal_end_str);
 
-		$single_date_events = array();
-		$multi_date_events = array();
+		$this->single_date_events = array();
+		$this->multi_date_events = array();
 		// cycle thru each event and put each in a bucket
 		foreach($events as $event) {
 			$event_start = $this->getDateClassification($event['start_dt']);
 			$event_end = $this->getDateClassification($event['end_dt']);
 			// if event start and end date are the same, save it in the single bucket
 			if($event_start === $event_end) {
-				$single_date_events[$event_start][] = $event;
+				$this->single_date_events[$event_start][] = $event;
 			}
 			// otherwise, save it as a multi day event stored with all others sharing the same day
 			else {
 				$dt_iter = new DateTime($event_start);
 				$dt_str = $dt_iter->format('Y-m-d');
 				do {
-					$multi_date_events[$dt_str][] = $event;
+					$this->multi_date_events[$dt_str][] = $event;
 					$dt_iter->add($day_interval);
 					$dt_str = $dt_iter->format('Y-m-d');
 				} while ( $dt_str <= $event_end );
 			}
 		}
+	}
 
-		$dt_iter = $start_date;
+	// returns a DateTime object representing the first day on the calendar
+	public function getFirstDate() {
+		return clone $this->start_date;
+	}
+
+	// returns a DateTime object representing the last day on the calendar
+	public function getLastDate() {
+		$dt = clone $this->start_date;
+		return $dt->add(new DateInterval('P13D'));
+	}
+
+	public function getAnchorDate() {
+		return clone $this->anchor_date;
+	}
+
+	public function display() {
+		// determine the calendar dates to cycle through
+		$day_interval = new DateInterval('P1D');
+		$dt_iter = $this->start_date;
 		$wk1_days = array();
 		$i = 0;
 		for(;$i < 7;$i++) {
-			$wk1_days[] = $dt_iter->add($day_interval)->format('Y-m-d');
+			$wk1_days[] = $dt_iter->format('Y-m-d');
+			$dt_iter->add($day_interval);
 		}
 		$wk2_days = array();
 		for(;$i < 14;$i++) {
-			$wk2_days[] = $dt_iter->add($day_interval)->format('Y-m-d');
+			$wk2_days[] = $dt_iter->format('Y-m-d');
+			$dt_iter->add($day_interval);
 		}
 
-		print '<ul>';
-		foreach($wk1_days as $day) {
-			print '<li>' . $day ; 
-			if(array_key_exists($day,$single_date_events)) {
-				print '<ul>';
-				foreach($single_date_events[$day] as $event) {
-					print '<li>' . $event['name'] . '</li>';
-				}
-				print '</ul>';
-			}
-			if(array_key_exists($day,$multi_date_events)) {
-				print '<ul>';
-				foreach($multi_date_events[$day] as $event) {
-					print '<li><i>' . $event['name'] . '</i></li>';
-				}
-				print '</ul>';
-			}
-			print '</li>';
-		}
+		// begin actual HTML rendering
+		print '<ul id="wk1-list">';
+		$this->printWeek($wk1_days);
 		print '</ul>';
 
-		print '<ul>';
-		foreach($wk2_days as $day) {
-			print '<li>' . $day ; 
-			if(array_key_exists($day,$single_date_events)) {
-				print '<ul>';
-				foreach($single_date_events[$day] as $event) {
-					print '<li>' . $event['name'] . '</li>';
-				}
-				print '</ul>';
-			}
-			if(array_key_exists($day,$multi_date_events)) {
-				print '<ul>';
-				foreach($multi_date_events[$day] as $event) {
-					print '<li><i>' . $event['name'] . '</i></li>';
-				}
-				print '</ul>';
-			}
-			print '</li>';
-		}
+		print '<ul id="wk2-list">';
+		$this->printWeek($wk2_days);
 		print '</ul>';
+	}
+
+	private function printWeek($day_list) {
+		foreach($day_list as $day) {
+			// link to eventlist.php?date=$day
+			$today = date('Y-m-d');
+			$relative_class = '';
+			if($day < $today) {
+				$relative_class = 'past';
+			}
+			elseif($day == $today) {
+				$relative_class = 'today';
+			}
+			else {
+				$relative_class = 'future';
+			}
+
+			$day_num = date('d',strtotime($day));
+			print "<li id='events-list-$day' class='day-list $relative_class'>";
+			print "<a href='eventlist.php?date=$day'>$day_num</a>";
+			if(array_key_exists($day,$this->single_date_events)) {
+				print '<ul class="single-day-events">';
+				$this->printEventList($this->single_date_events[$day]);
+				print '</ul>';
+			}
+			if(array_key_exists($day,$this->multi_date_events)) {
+				print '<ul class="multi-day-events">';
+				$this->printEventList($this->multi_date_events[$day]);
+				print '</ul>';
+			}
+			print "</li>\n";
+		}
+	}
+	private function printEventList($events) {		
+		foreach($events as $event) {
+			$id = $event['id'];
+			$name = $event['name'];
+			print "<li><a href='events.php?eid=$id'>$name</a></li>";
+		}
 	}
 
 	// return an array of events, each event has the following fields:
 	//  - id, name, start_dt, end_dt
-	private function findAllEvents($start_date,$end_date) {
+	// Note that the $end day is exclusive (i.e. function will return
+	//  events up to BUT NOT INCLUDING that day).
+	private function findAllEvents($start,$end) {
 		// TODO: when doing a DB query, keep the whole 4 AM cutoff thing in mind?
 		$sql = "SELECT event_id, event_name, event_start_date, event_start_time, event_end_date, event_end_time 
 					FROM wp_em_events 
-					WHERE event_start_date <= :enddate AND event_end_date >= :startdate";
+					WHERE event_start_date < :enddate AND event_end_date >= :startdate
+					ORDER BY event_end_date ASC";
 
 		try {
 			$pdo = new PDO('mysql:host='.OIP_DB_HOST.';dbname='.OIP_DB_NAME, 
 							OIP_DB_USER, OIP_DB_PASSWORD);
 			$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 			$statement = $pdo->prepare($sql);
-			$statement->execute(array('startdate'=>$start_date,'enddate'=>$end_date));;
+			$statement->execute(array('startdate'=>$start,'enddate'=>$end));
 		}
 		catch(PDOException $e) {  
 		    die('PDO MySQL error: ' . $e->getMessage());  
@@ -141,4 +167,3 @@ class TwoWeekCalendar {
 	}
 }
 
-new TwoWeekCalendar('2011-09-24');
---
+++ calendar.php
@@ -0,0 +1,71 @@
+<?php
+require_once 'calendar.class.php';
+
+if(!array_key_exists('anchor',$_GET)) {
+	$anchor_date_str = date('Y-m-d');
+}
+else {
+	$anchor_date_str = $_GET['anchor'];
+}
+
+$ajax = FALSE;
+if(array_key_exists('ajax',$_GET)) {
+	$ajax = $_GET['ajax'] == TRUE;
+}
+
+$cal = new TwoWeekCalendar($anchor_date_str);
+$twowks = new DateInterval('P14D');
+
+$prev_anchor = $cal->getAnchorDate()->sub($twowks)->format('Y-m-d');
+$next_anchor = $cal->getAnchorDate()->add($twowks)->format('Y-m-d');
+
+if(!$ajax): ?>
+<div id="cal-container">
+<?php endif; ?>
+
+<div id="cal-header">
+	<span id="cal-datespan-text"><?php echo $cal->getFirstDate()->format('M j'); ?> - <?php echo $cal->getLastDate()->format('M j'); ?></span>
+	<span id="cal-buttons">
+		<a href="calendar.php?anchor=<?php echo $prev_anchor; ?>" class="cal-nav-link" id="cal-nav-prev">&lt;</a>
+		<a href="calendar.php?anchor=<?php echo $next_anchor; ?>" class="cal-nav-link" id="cal-nav-next">&gt;</a>
+	</span>
+</div>
+
+<?php
+$cal->display();
+
+if(!$ajax): ?>
+</div>
+<?php endif; ?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" type="text/javascript"></script>
+<script>
+
+/* Function designed to be called when a link is clicked to short circuit the 
+ *	normal browser of following the link and instead call the link's "href" via AJAX
+ * Arguments:
+ *  element - jQuery element for the link clicked
+ *  callback - function to execute on successful AJAX return
+ */
+function ajaxClick(element,callback) {
+	$.ajax( {
+		url: element.attr('href') + '&ajax=1',
+		success: callback,
+		datatype: 'html' } );
+	return false;
+}
+
+// when DOM is loaded, add the click events
+$(document).ready(
+	function() {
+		$('#cal-nav-prev').click( function() { 
+			return ajaxClick( 	$('#cal-nav-prev'), 
+								function(data) { $('#cal-container').html(data); } ) } 
+		);
+		$('#cal-nav-next').click( function() { 
+			return ajaxClick( 	$('#cal-nav-next'), 
+								function(data) { $('#cal-container').html(data); } ) }
+		);
+	}
+);
+</script>
