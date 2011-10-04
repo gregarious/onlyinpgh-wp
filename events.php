65028bb69e6ba5a98e324adcce4309d21841d634
Greg Nicholas <greg.nicholas@gmail.com>
2011-10-04 19:25:52 -0400

    Fixed bug from multiple definitions of onSameDay

--- events.php
+++ events.php
@@ -3,6 +3,19 @@
 require_once 'etc/config.php';
 require_once 'include/eventsearcher.class.php';
 
+if(!function_exists('onSameDay')) {
+	function onSameDay($dt1,$dt2) {
+		if( $dt1->format('H:i') < '04:01' ) {
+			$dt1->sub(new DateInterval('P1D'));
+		}
+		if( $dt2->format('H:i') < '04:01' ) {
+			$dt2->sub(new DateInterval('P1D'));
+		}
+
+		return $dt1->format('Y-m-d') == $dt2->format('Y-m-d');
+	}
+}
+
 if(!array_key_exists('eid',$_GET)) {
 	die();
 }
@@ -20,20 +33,3 @@ if( is_user_logged_in() ) {
 
 $searcher->filterByEventId($eid);
 $results = $searcher->runQuery(0,1);
-
-
-function onSameDay($dt1,$dt2) {
-	if( $dt1->format('H:i') < '04:01' ) {
-		$dt1->sub(new DateInterval('P1D'));
-	}
-	if( $dt2->format('H:i') < '04:01' ) {
-		$dt2->sub(new DateInterval('P1D'));
-	}
-
-	return $dt1->format('Y-m-d') == $dt2->format('Y-m-d');
-}
-
-
-/*echo '<pre>';
-echo print_r($results);
-echo '</pre>';*/
