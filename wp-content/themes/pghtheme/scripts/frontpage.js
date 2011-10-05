91637ce2fdd98dd7d2fa76fb782e7f596bed6fef
Greg Nicholas <greg.nicholas@gmail.com>
2011-10-05 14:05:12 -0400

    added messages for empty map searches results

--- wp-content/themes/pghtheme/scripts/frontpage.js
+++ wp-content/themes/pghtheme/scripts/frontpage.js
@@ -9,6 +9,19 @@ function initializeEventResults() {
 		);
 }
 
+function displayNoResultsMessage(jquery_obj) {
+	jquery_obj.html( '<div id="noresult">' +
+					'<b>Sorry! None of our event listings matched your search.</b><br/>' +
+					'<br/>' +
+					'<b>You could try...</b><br/>' +
+					'<p>Searching in a different neighborhood<br/>' +
+					'Searching in all neighborhoods<br/>' +
+					'Searching with a different keyword<br/>' +
+					'Removing the keyword from your search<br/>' +
+					'Or searching with a different date range</p>' +
+				   '</div>' );
+}
+
 // as soon as DOM is loaded, initialize the map and start the search
 jQuery(document).ready(function() {
 	
