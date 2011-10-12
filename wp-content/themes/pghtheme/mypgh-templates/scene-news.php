e909c35f053cb6182b06bce8a1b56f0912cfca67
Lara Schenck <laras126@gmail.com>
2011-10-12 13:42:13 -0400

    RSS feed styles and dummy content

--- wp-content/themes/pghtheme/mypgh-templates/scene-news.php
+++ wp-content/themes/pghtheme/mypgh-templates/scene-news.php
@@ -1,5 +1,5 @@
-	
-<h2>What's Happening?</h2> 
+		
+<h2>Scene News</h2> 
 <!--<span id="cal-buttons" class="alignright">
 	<a href="" class="day-nav-link" id="cal-nav-prev">&larr; Previous</a>
 	<a href="" class="day-nav-link" id="cal-nav-next">Next &rarr;</a>
@@ -8,39 +8,63 @@
 <ul class="rss-list">
 	
 		<li>
-			<div id="rss-img">
+<!--			<div id="rss-img">
 				<img src="http://profile.ak.fbcdn.net/hprofile-ak-ash2/277151_258604634157874_1077056_s.jpg" class="alignleft">
-			</div>
+			</div> -->
 			<h3 class="rss-title">
 				An interesting article about music
 			</h3>
 
-			<div id="">
-				<p class="">Source:</h4>
-				<h4 class="">Mr. Small Theatre</h4>
-				<p class="article-date">March 12, 1989</p> 
-			</div> <!-- #el-host-address -->
+			<p class="rss-postedon">Posted on:<h4 class="rss-blog"> OMGPGH!</h4>
+			<p class="rss-date">March 12, 1989</p>
 
-			<p class="">Bicycle rights helvetica mlkshk fanny pack +1 leggings, 8-bit artisan freegan stumptown wes anderson butcher cardigan organic gluten-free. Seitan VHS cliche tofu, vice keytar retro you probably haven't heard of them blog shoreditch +1 messenger bag sustainable american apparel gentrify...</p>
+			<p class="rss-desc">Bicycle rights helvetica mlkshk fanny pack +1 leggings, 8-bit artisan freegan stumptown wes anderson butcher cardigan organic gluten-free. Seitan VHS cliche tofu, vice keytar retro you probably haven't heard of them...</p>
 
 		</li>
 		
 
 		<li>
-			<div id="rss-img">
+<!--			<div id="rss-img">
 				<img src="http://profile.ak.fbcdn.net/hprofile-ak-ash2/277151_258604634157874_1077056_s.jpg" class="alignleft">
-			</div>
+			</div> -->
 			<h3 class="rss-title">
-				An interesting article about music
+				Shadyside is the new Berlin
 			</h3>
 
-			<div id="">
-				<p class="">Source:</h4>
-				<h4 class="">Mr. Small Theatre</h4>
-				<p class="article-date">March 12, 1989</p> 
-			</div> <!-- #el-host-address -->
+			<p class="rss-postedon">Posted on:<h4 class="rss-blog"> BoringPGH</h4>
+			<p class="rss-date">March 12, 1989</p>
 
-			<p class="">Bicycle rights helvetica mlkshk fanny pack +1 leggings, 8-bit artisan freegan stumptown wes anderson butcher cardigan organic gluten-free. Seitan VHS cliche tofu, vice keytar retro you probably haven't heard of them blog shoreditch +1 messenger bag sustainable american apparel gentrify...</p>
+			<p class="rss-desc">Bicycle rights helvetica mlkshk fanny pack +1 leggings, 8-bit artisan freegan stumptown wes anderson butcher cardigan organic gluten-free. Seitan VHS cliche tofu, vice keytar retro you probably haven't heard of them...</p>
+
+		</li>
+
+				<li>
+<!--			<div id="rss-img">
+				<img src="http://profile.ak.fbcdn.net/hprofile-ak-ash2/277151_258604634157874_1077056_s.jpg" class="alignleft">
+			</div> -->
+			<h3 class="rss-title">
+				Lots of cool kids in the burgh
+			</h3>
+
+			<p class="rss-postedon">Posted on:<h4 class="rss-blog"> Post Gazette</h4>
+			<p class="rss-date">March 12, 1989</p>
+
+			<p class="rss-desc">Bicycle rights helvetica mlkshk fanny pack +1 leggings, 8-bit artisan freegan stumptown wes anderson butcher cardigan organic gluten-free. Seitan VHS cliche tofu, vice keytar retro you probably haven't heard of them...</p>
+
+		</li>
+
+				<li>
+<!--			<div id="rss-img">
+				<img src="http://profile.ak.fbcdn.net/hprofile-ak-ash2/277151_258604634157874_1077056_s.jpg" class="alignleft">
+			</div> -->
+			<h3 class="rss-title">
+				Wow, there are a lot of concerts in Pittsburgh
+			</h3>
+
+			<p class="rss-postedon">Posted on:<h4 class="rss-blog"> IheartPgh</h4>
+			<p class="rss-date">March 12, 1989</p>
+
+			<p class="rss-desc">Bicycle rights helvetica mlkshk fanny pack +1 leggings, 8-bit artisan freegan stumptown wes anderson butcher cardigan organic gluten-free. Seitan VHS cliche tofu, vice keytar retro you probably haven't heard of them...</p>
 
 		</li>
 
